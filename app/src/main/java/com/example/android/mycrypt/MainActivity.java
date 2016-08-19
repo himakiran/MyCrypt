16fd4a734a5b53910265073cb4b5063fe4c98b73
chkumar <himakiran@gmail.com>
2016-08-19 23:14:13 +0530

    Wrote code to encrypt and decrypt the text entered in clearText or cipherext box respectively.
    one issue is the default text in clearText is not getting erased when user starts typing.
    
    the encrypted ciphertext is not being displayed.
    
    code is compiling without errors

--- app/src/main/java/com/example/android/mycrypt/MainActivity.java
+++ app/src/main/java/com/example/android/mycrypt/MainActivity.java
@@ -2,6 +2,23 @@ package com.example.android.mycrypt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +27,142 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * below variables hold the clear,cipher text and a secret key.
+     * for now the sceret key is genr from a hardcode passwd in function @generatekey.
+     */
+    public static String clearText;
+    public static String cipherText;
+    public static SecretKey secret;
+    public static boolean clearBool;
+    public static boolean cipherBool;
+
+    /**
+     *
+     * @param view gets the clearText view and then checks if text is available.
+     *             if yes sets the bool var to true and stores the text in clearText var.
+     */
+    public void readEditText(View view)
+    {
+        EditText editText = (EditText) findViewById(R.id.clearText);
+        if(!(editText.getText().toString()==""))
+        {
+            clearText = editText.getText().toString();
+            clearBool = true;
+            Log.d("clearText",clearText);
+        }
+        else
+        {
+            clearBool = false;
+        }
+    }
+
+    /**
+     *
+     * @param view gets the cryptText view and then checks if text is available.
+     *             if yes sets the bool var to true and stores the text in cipherText var.
+     */
+    public void readCipherText(View view)
+    {
+        EditText editText = (EditText) findViewById(R.id.cryptText);
+        if(!(editText.getText().toString()==""))
+        {
+            cipherText = editText.getText().toString();
+            cipherBool = true;
+            Log.d("clearText",clearText);
+        }
+        else
+        {
+            cipherBool = false;
+        }
+
+    }
+
+    public static SecretKey generateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String pwd ="password";
+        SecretKey secret;
+        return secret = new SecretKeySpec(pwd.getBytes(), "AES");
+    }
+
+    public static String encryptMsg(String message, SecretKey secret) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+/* Encrypt the message. */
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        byte[] cipherText = cipher.doFinal(message.getBytes("UTF-8"));
+        String encryptString = new String(cipher.doFinal(cipherText), "UTF-8");
+        return encryptString;
+    }
+    public static String decryptMsg(byte[] cipherText, SecretKey secret) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidParameterSpecException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+
+    /* Decrypt the message, given derived encContentValues and initialization vector. */
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        String decryptString = new String(cipher.doFinal(cipherText), "UTF-8");
+        return decryptString;
+    }
+
+    public static void encrypt(){
+
+        try {
+            secret = generateKey();
+        }
+        catch (Exception a) {
+            Log.d("algo", a.getMessage());
+
+        }
+        try{
+            cipherText = encryptMsg(clearText,secret);
+        }
+        catch(Exception a){
+            Log.d("algo",a.getMessage());
+
+        }
+
+
+
+    }
+    public static void decrypt() {
+
+        try {
+            secret = generateKey();
+        } catch (Exception a) {
+            Log.d("algo", a.getMessage());
+
+        }
+        try {
+            byte[] cipherTextBytes = cipherText.getBytes("UTF-8");
+            clearText = decryptMsg(cipherTextBytes, secret);
+        } catch (Exception a) {
+            Log.d("algo", a.getMessage());
+
+        }
+    }
+/**
+ * This method is called when the order button is clicked.
+ */
+    public static void encryptOrDecrypt(View view){
+
+        if(clearBool && !cipherBool){
+
+            encrypt();
+        }
+        else if(!clearBool && cipherBool){
+            decrypt();
+        }
+
+        else if(!clearBool && !cipherBool){
+            //throw a messagebox asking user to enter text
+        }
+        else
+        {
+            //throw a message box asking user to clear selection
+        }
+
+    }
+
+
+
 }
